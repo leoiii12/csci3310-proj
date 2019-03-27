2a2be34f401957ac9565c7e2c72c432fb71262b4
Leo Choi <choimankin@gmail.com>
2019-03-27 15:42:52 +0800

    Added http module

--- android/app/src/main/java/com/example/a3310_proj_01/MainActivity.java
+++ android/app/src/main/java/com/example/a3310_proj_01/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.a3310_proj_01;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.swagger.client.apis.AuthApi;
+import io.swagger.client.models.AuthenticateInput;
+import io.swagger.client.models.AuthenticateOutput;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                AuthApi apiInstance = new AuthApi();
+                AuthenticateInput input = new AuthenticateInput("choimankin@gmail.com", "12345678");
+
+                try {
+                    AuthenticateOutput result = apiInstance.apiAuthAuthenticate(input);
+                    System.out.println(result.getAccessToken());
+                } catch (Exception e) {
+                    System.err.println("Exception when calling AuthApi#apiAuthAuthenticate");
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
     }
 
 }
